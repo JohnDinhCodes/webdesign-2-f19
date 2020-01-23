91297ce0c514c3ff37b496c1d063515f5a85a85b
John Dinh <37444633+JohnDinhCodes@users.noreply.github.com>
2020-01-22 21:22:49 -0500

    add initial files for nav-bar

---
+++ src/components/nav-bar/nav-bar.component.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./nav-bar.styles.css";
+import {
+	Link,
+	DirectLink,
+	Element,
+	Events,
+	animateScroll as scroll,
+	scrollSpy,
+	scroller
+} from "react-scroll";
+
+export const NavBar = () => {
+	<nav>
+		<ul>
+			<li></li>
+		</ul>
+	</nav>;
+};
