a3fef773f0a4c5ab3b653a973b56485d88ca9851
John Dinh <johnduydinh@gmail.com>
2020-01-24 23:47:05 -0500

    Updates

---
+++ precache-manifest.1725c1234f7cce8592b1364fa84c10a0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ec3a4b92aa864cd4b2dd3b9697c8542",
+    "url": "/webdesign-2-f19/index.html"
+  },
+  {
+    "revision": "a15a8fff3f62f18e9f1c",
+    "url": "/webdesign-2-f19/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "b775b0ac7a2c2cb7b8e8",
+    "url": "/webdesign-2-f19/static/css/main.18c1c735.chunk.css"
+  },
+  {
+    "revision": "a15a8fff3f62f18e9f1c",
+    "url": "/webdesign-2-f19/static/js/2.48c1dd97.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/webdesign-2-f19/static/js/2.48c1dd97.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b775b0ac7a2c2cb7b8e8",
+    "url": "/webdesign-2-f19/static/js/main.4b236829.chunk.js"
+  },
+  {
+    "revision": "caab0ce30dd06369d8f5",
+    "url": "/webdesign-2-f19/static/js/runtime-main.6925809e.js"
+  },
+  {
+    "revision": "968a08936d30afb26e3a4b54d5543302",
+    "url": "/webdesign-2-f19/static/media/logo.968a0893.svg"
+  },
+  {
+    "revision": "8204a4b201c9c9d08feac58310892c92",
+    "url": "/webdesign-2-f19/static/media/logotype.8204a4b2.svg"
+  }
+]);
